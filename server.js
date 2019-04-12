9610bf265ce937e76c60965c5aee0d5b600753ab
matthewmerks <matthew@pbos.co.za>
2019-04-12 20:14:24 +0200

    Implementation of JWT and passport

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,7 +9,11 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-//DB config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB config
 const db = require('./config/keys').mongoURI;
 
 // Connect to mongodb
@@ -16,7 +22,11 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport strategy
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users/', users);
